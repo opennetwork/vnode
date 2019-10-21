cb885be8a84c88653968b7fd9289c8024b9ebd3c
Fabian Cook <hello@fabiancook.dev>
2019-10-21 13:29:17 +1300

    scope fragment morE

--- src/fragment.ts
+++ src/fragment.ts
@@ -2,4 +2,4 @@
  * `unique symbol` to represent a {@link FragmentVNode}, this will be used on the {@link VNode.reference} property.
  * The {@link FragmentVNode} should be ignored and the {@link VNode.children} values should be used in its place
  */
-export const Fragment = Symbol.for("VNode.Fragment");
+export const Fragment = Symbol.for("@opennetwork/vnode/fragment");
